33aad610e7bcd374b28bf612acc62c326ef4ae1c
Malihanaz <gmalihanaz@gmail.com>
2024-01-19 18:30:20 +0530

    bankloan project

---
+++ WebAppBankLoan/Models/Errorviewmodel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppBankLoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
